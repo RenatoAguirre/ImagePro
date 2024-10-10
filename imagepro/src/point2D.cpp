b138a53e14da7d6f05076ba0816618497b376053
RenatoAguirre <raaguirre1@miuandes.cl>
2024-10-10 13:21:33 -0300

    fix: fixed point2D class

--- imagepro/src/point2D.cpp
+++ imagepro/src/point2D.cpp
@@ -1,27 +1,27 @@
 /* implementation of the class Point2D
  */
 
- namespace image{
-    Point2D::Point2D(): x(0), y(0) {
+#include "image/point2D.hpp"
+namespace image {
+    Point2D::Point2D() : x(0), y(0) {}
 
-    }
+    Point2D::Point2D(int _x, int _y) : x(_x), y(_y) {}
 
-	Point2D(int _x, int _y): x(_x), y(_y){
-
-    }
-    void Point2D::setX(int _x){
+    void Point2D::setX(int _x) {
         x = _x;
     }
-    void Point2D::setY(int _y){
+
+    void Point2D::setY(int _y) {
         y = _y;
     }
-    int Point2D::getX(){
+
+    int Point2D::getX() {
         return x;
     }
-    int Point2D::getY(){
+
+    int Point2D::getY() {
         return y;
     }
-    virtual Point_2D::~Point2D(){
-        
-    }
- }
+
+    Point2D::~Point2D() {}
+}
